4e937ed4c883d90faa6574d5e3684f8c9a6c01dd
Mr.Marble <mohammadrezamarble@gmail.com>
2026-08-30 20:49:22 +0330

    fix(gradle): remove kotlin.android plugin – not needed with AGP 9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "9.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
