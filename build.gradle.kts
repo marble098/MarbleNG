3c5ed2f25a66d391fac6c20bcb76e5fdb041f65d
Mr.Marble <mohammadrezamarble@gmail.com>
2026-08-30 21:02:44 +0330

    fix(gradle): apply hilt after android plugin – fixes BaseExtension error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "9.2.1" apply false
+    id("com.android.application")         version "9.2.1"  apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
-    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    kotlin("kapt")                         version "2.3.10" apply false
+    id("com.google.dagger.hilt.android")  version "2.56.2" apply false
 }
